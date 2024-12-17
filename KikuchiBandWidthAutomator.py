f9805d6a33cb2270809d95430410c4eae84596cf
kvmani <kvmani@gmail.com>
2024-12-17 15:34:38 +0530

    main now recieves pc camera tilt etc from .h5 header

--- KikuchiBandWidthAutomator.py
+++ KikuchiBandWidthAutomator.py
@@ -289,65 +289,36 @@ class CustomKikuchiPatternSimulator(KikuchiPatternSimulator):
 def main():
     # Load configuration
     start_time = time.time()
-    #config = load_config(file_path="bandDetectorOptions.yml")
-    config = load_config(file_path="bandDetectorOptionsMagnetite.yml")
-    debug = config.get("debug", False)
+    config = load_config(file_path="bandDetectorOptions.yml")
+    #config = load_config(file_path="bandDetectorOptionsMagnetite.yml")
+    #config = load_config(file_path="bandDetectorOptionsHeamatite.yml")
+    
     data_path = config.get("h5_file_path", "path_to_default_file.h5")
     output_dir = os.path.dirname(data_path)
-    base_name = os.path.splitext(os.path.basename(data_path))[0]
-
-    # Create a backup of the original file
-
-    # Copy the backup to a temp file for modifications
+    base_name, ext = os.path.splitext(os.path.basename(data_path))
+    
+    # Check if the input file ends with .oh5 and create a .h5 copy
+    if ext == ".oh5":
+        new_data_path = os.path.join(output_dir, f"{base_name}.h5")
+        shutil.copy(data_path, new_data_path)
+        logging.info(f"Copied .oh5 file to new .h5 file: {new_data_path}")
+        data_path = new_data_path  # Update data_path to point to the new .h5 file
+    
+    temp_data_path = ut.create_temp_file(data_path)
     modified_data_path = os.path.join(output_dir, f"{base_name}_modified.h5")
-
-    logging.info(f"Copied backup file to: {modified_data_path}")
-
-    # Reorder patterns if the flag is enabled
-    if config.get("reorder_patterns_from_hdf", False):
-        temp_data_path = ut.create_temp_file(data_path)
-        shutil.copy(temp_data_path, modified_data_path)
-        with h5py.File(modified_data_path, "r") as h5file:
-            target_dataset_name = next(name for name in h5file if name not in ["Manufacturer", "Version"])
-        ut.reorder_patterns_in_hdf(modified_data_path, target_dataset_name,debug=debug)
-        data_path = modified_data_path
-        #modified_data_path = temp_data_path
-    else:
-        shutil.copy(data_path, modified_data_path)
-        logging.info(f"Copied HDF5 file to: {modified_data_path}")
-
-
-    if data_path.endswith(".oh5"):
-        h5_data_path = os.path.join(output_dir, f"{base_name}.h5")
-        shutil.copy(data_path, h5_data_path)
-        logging.info(f"Copied .oh5 file to: {h5_data_path}")
-        data_path = h5_data_path
 
     in_ang_path = os.path.join(output_dir, f"{base_name}.ang")
     out_ang_path = os.path.join(output_dir, f"{base_name}_modified.ang")
-
-    #exit(-100)
-
-
+    shutil.copy(data_path, modified_data_path)
+    logging.info(f"Copied HDF5 file to: {modified_data_path}")
 
     crop_start, crop_end = config.get("crop_start", 5), config.get("crop_end", 25)
-
+    debug = config.get("debug", False)
     desired_hkl = config.get("desired_hkl", "111")
 
     # Load dataset and optional cropping based on debug flag
-    # logging.info(f"Loading dataset from: {data_path}")
+    logging.info(f"Loading dataset from: {data_path}")
     s = kp.load(data_path, lazy=False)
-    # iq = s.get_image_quality(normalize=True)  # Default
-    #
-    # s.xmap.plot(
-    #     iq.ravel(),
-    #     cmap="gray",
-    #     colorbar=True,
-    #     colorbar_label="Image quality, $Q$",
-    #     remove_padding=True,
-    # )
-    # plt.show()
-    # exit(-100)
 
     if debug:
         logging.info("Debug mode enabled: Cropping data for faster processing.")
@@ -371,10 +342,24 @@ def main():
     )
     hkl_list = config["hkl_list"]
     pc = config.get("pc", [0.545, 0.610, 0.6863])
-
+    header_data = ut.extract_header_data(modified_data_path)
+    
     # Detector setup and pattern extraction
     sig_shape = s.axes_manager.signal_shape[::-1]
-    det = kp.detectors.EBSDDetector(sig_shape, sample_tilt=70, convention="edax", pc=pc)
+    det = kp.detectors.EBSDDetector(
+            sig_shape, 
+            sample_tilt=float(header_data.get("Sample Tilt", 0.0)),
+            tilt=float(header_data.get("Camera Elevation Angle", 0.0)),
+            azimuthal=float(header_data.get("Camera Azimuthal Angle", 0.0)),
+            convention="edax",
+            pc=tuple(header_data.get("pc", (0.0, 0.0, 0.0)))
+            )
+
+    # det = kp.detectors.EBSDDetector(sig_shape, 
+    #                                 sample_tilt=float(header_data.get("Sample Tilt", 0.0)), 
+    #                                 camera_tilt=float(header_data.get("Camera Elevation Angle", 0.0)),
+    #                                 azimuthal_angle=float(header_data.get("Camera Azimuthal Angle", 0.0))
+    #                                 convention="edax", pc=header_data.get("pc",(0.,0.,0.)))
 
     # PhaseList and indexing
     indexer = det.get_indexer(phase_list, hkl_list, nBands=10, tSigma=2, rSigma=2)
@@ -392,6 +377,17 @@ def main():
     markers, grouped_kikuchi_dict_list = sim.as_markers(kikuchi_line_labels=True, desired_hkl=config["desired_hkl"])
     s.add_marker(markers, plot_marker=False, permanent=True)
     logging.info("Completed band identification for width estimation. Now starting the band width estimation!")
+    
+    v_ipf = Vector3d.xvector()
+    sym = xmap.phases[0].point_group
+    ckey = plot.IPFColorKeyTSL(sym, v_ipf)
+    rgb_x = ckey.orientation2color(xmap.rotations)
+    maps_nav_rgb = kp.draw.get_rgb_navigator(rgb_x.reshape(xmap.shape + (3,)))
+    s.plot(maps_nav_rgb)
+    plt.show()
+
+    exit(-200)
+
 
     # Call the process_kikuchi_images function
     ebsd_data = s.data  # EBSD dataset where each (row, col) contains the Kikuchi pattern (2D numpy array)
@@ -399,7 +395,6 @@ def main():
     output_excel_path = os.path.join(output_dir, f"{base_name}_bandOutputData.xlsx")
     filtered_excel_path = os.path.join(output_dir, f"{base_name}_filtered_band_data.xlsx")
     save_results_to_excel(processed_results, output_excel_path,filtered_excel_path)
-
 
     df = pd.read_excel(filtered_excel_path)
     if "Band Width" not in df.columns:
@@ -434,39 +429,31 @@ def main():
         band_strain_array = (band_width_array - desired_hkl_ref_width) / desired_hkl_ref_width
         elastic_modulus = float(config["elastic_modulus"])
         band_stress_array = band_strain_array * elastic_modulus
-        if not config["skip_ang_processing"]:
-            angInputDict = {"IQ":band_width_array, "PRIAS_Bottom_Strip":band_strain_array,
-                            "PRIAS_Center_Square":band_stress_array,
-                            "PRIAS_Top_Strip":psnr_array,
-                            }
+        angInputDict = {"IQ":band_width_array, "PRIAS_Bottom_Strip":band_strain_array,
+                        "PRIAS_Center_Square":band_stress_array,
+                        "PRIAS_Top_Strip":psnr_array,
+                        }
 
-            ut.modify_ang_file(in_ang_path, "band_width", IQ=band_width_array)
-            ut.modify_ang_file(in_ang_path, "strain", IQ=band_strain_array)
-            ut.modify_ang_file(in_ang_path, "stress", IQ=band_stress_array)
-            ut.modify_ang_file(in_ang_path, "psnr", IQ=psnr_array)
+        ut.modify_ang_file(in_ang_path, "band_width", IQ=band_width_array)
+        ut.modify_ang_file(in_ang_path, "strain", IQ=band_strain_array)
+        ut.modify_ang_file(in_ang_path, "stress", IQ=band_stress_array)
+        ut.modify_ang_file(in_ang_path, "psnr", IQ=psnr_array)
 
-            logging.info("Succesfully wrote band_width in IQ, strain in PRIAS_Bottom_Strip, stress in PRIAS_Center_Square, psnr in PRIAS_Top_Strip of modified ang file!!")
+        logging.info("Succesfully wrote band_width in IQ, strain in PRIAS_Bottom_Strip, stress in PRIAS_Center_Square, psnr in PRIAS_Top_Strip of modified ang file!!")
         h5file.create_dataset(f"/{target_dataset_name}/EBSD/Data/Band_Width", data=band_width_array)
         h5file.create_dataset(f"/{target_dataset_name}/EBSD/Data/psnr", data=psnr_array)
         h5file.create_dataset(f"/{target_dataset_name}/EBSD/Data/strain", data=band_strain_array)
         h5file.create_dataset(f"/{target_dataset_name}/EBSD/Data/stress", data=band_stress_array)
 
         logging.info("Added Band_Width data to /Nickel/EBSD/Data/Band_Width in modified HDF5 file.")
-        logging.info("Process completed. Results saved to Excel files and modified .ang file.")
+    logging.info("Process completed. Results saved to Excel files and modified .ang file.")
 
     end_time = time.time()  # End timing
     logging.info(f"The total processing time is : {end_time - start_time} seconds")
 
     # Optional visualization
 
-    v_ipf = Vector3d.xvector()
-    sym = xmap.phases[0].point_group
-    ckey = plot.IPFColorKeyTSL(sym, v_ipf)
-    rgb_x = ckey.orientation2color(xmap.rotations)
-    maps_nav_rgb = kp.draw.get_rgb_navigator(rgb_x.reshape(xmap.shape + (3,)))
-    s.plot(maps_nav_rgb)
-    plt.show()
-
+    
 
 if __name__ == "__main__":
     main()
